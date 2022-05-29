25e7d4b0cfd6687ff3fd2b91474fac86081dcc16
ProgramWarfare <95079489+ProgramWarfare@users.noreply.github.com>
2022-05-29 16:32:30 +0200

    Add files via upload

---
+++ js/domjs.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var imageCounter= 1;
+
+ShowDiv(imageCounter);
+
+function changeImage(m) {
+
+    ShowDiv(imageCounter = imageCounter + m);
+
+}
+
+function ShowDiv (n) {
+
+    var i;
+
+    var imageArray = document.getElementsByClassName ("mySlides");
+
+    if (n > imageArray.length) { 
+
+        imageCounter = 1;
+
+}
+
+if (n < 1) {
+
+    imageCounter = imageArray.length;
+
+}
+
+for (i = 0; i < imageArray.length; i++) {
+
+    imageArray[i].style.display = "none";
+
+}
+
+imageArray[imageCounter - 1].style.display = "block";
+
+}
